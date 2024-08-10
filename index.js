2e80091c363c9ca13a6ef7c17aff54974fde0571
Olivia Waithira <oliviawaithira7@gmail.com>
2024-08-11 00:28:01 +0300

    javascript

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const btnEl = document.getElementById("btn")
-
+const bmiInputEl = document.getElementById("bmi-reult")
 
 function calculateBMI(){
-    const heightValue = document.getElementById("height").value
+    const heightValue = document.getElementById("height").value / 100
     const weightValue = document.getElementById("weight").value
+
+    const bmiValue = weiightValue / (heightValue * heightValue)
+
+    console.log(bmiValue)
 }
 
 btnEl.addEventListener("click", calculateBMI)
