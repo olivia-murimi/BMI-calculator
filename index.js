ab83fe4a4aef9d36535df015c2bdce53fe56d859
Olivia Waithira <oliviawaithira7@gmail.com>
2024-08-11 00:34:47 +0300

    javascript

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const btnEl = document.getElementById("btn")
 const bmiInputEl = document.getElementById("bmi-reult")
-
+const weightconditiobEl = document.getElementById("weight-conditon")
 function calculateBMI(){
     const heightValue = document.getElementById("height").value / 100
     const weightValue = document.getElementById("weight").value
 
     const bmiValue = weiightValue / (heightValue * heightValue)
 
-    console.log(bmiValue)
+    
+    bmiInputEl.value = bmiValue;
+
+    if(bmiValue < 18.5){
+      weightConditionEl.innerText = "Under weight"
+    }
 }
 
 btnEl.addEventListener("click", calculateBMI)
