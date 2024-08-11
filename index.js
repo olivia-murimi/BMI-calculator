c9d0f06c83ed694fc2a7e2d09ef427630ef39eb6
Olivia Waithira <oliviawaithira7@gmail.com>
2024-08-12 01:07:37 +0300

    javascript

--- index.js
+++ index.js
@@ -11,16 +11,17 @@ function calculateBMI(){
     bmiInputEl.value = bmiValue;
 
     if(bmiValue < 18.5){
-      weightConditionEl.innerText = "Under weight"
+      weightConditionEl.innerText = "Under weight";
     } else if (bmiValue >=18.5 && bmiValue <= 24.9)
     {
-      weightConditionEl.innerText = "Normal weight"
+      weightConditionEl.innerText = "Normal weight";
     } else if (bmiValue >= 25 && bmiValue <29.9){
-      weightConditionEl.innerText = "Overweight"
+      weightConditionEl.innerText = "Overweight";
     } else if (bmiValue <=30){
-      weightConditionEl.innerText = "Under weight"
+      weightConditionEl.innerText = "Obesity";
     }
 }
 
 btnEl.addEventListener("click", calculateBMI)
+
 
