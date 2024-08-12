9cca26f92bdb6f26129e4726db5e041073a03ac4
Olivia Waithira <oliviawaithira7@gmail.com>
2024-08-13 00:28:57 +0300

    BMI

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const btnEl = document.getElementById("btn")
-const bmiInputEl = document.getElementById("bmi-reult")
-const weightconditionEl = document.getElementById("weight-conditon")
+const btnEl = document.getElementById("btn");
+const bmiInputEl = document.getElementById("bmi-result");
+const weightConditionEl = document.getElementById("weight-condition");
 function calculateBMI(){
-    const heightValue = document.getElementById("height").value / 100
-    const weightValue = document.getElementById("weight").value
+    const heightValue = document.getElementById("height").value / 100;
+    const weightValue = document.getElementById("weight").value;
 
-    const bmiValue = weiightValue / (heightValue * heightValue)
+    const bmiValue = weightValue / (heightValue * heightValue);
 
     
     bmiInputEl.value = bmiValue;
 
-    if(bmiValue < 18.5){
-      weightConditionEl.innerText = "Under weight";
+    if(bmiValue < 18.5) {
+      weightConditionEl.innerText = "Underweight";
     } else if (bmiValue >=18.5 && bmiValue <= 24.9)
     {
       weightConditionEl.innerText = "Normal weight";
     } else if (bmiValue >= 25 && bmiValue <29.9){
       weightConditionEl.innerText = "Overweight";
-    } else if (bmiValue <=30){
+    } else if (bmiValue >=30){
       weightConditionEl.innerText = "Obesity";
     }
 }
 
 btnEl.addEventListener("click", calculateBMI)
-
 
